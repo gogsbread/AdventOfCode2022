f3bfe11d3ae614ea7f8bcde3a025b865808f44a9
Antony Deepak Thomas <antonydeepak@gmail.com>
2022-12-03 19:40:11 -0800

    1ufs

--- f#/1.fsx
+++ f#/1.fsx
@@ -3,23 +3,22 @@
 open Common
 open System
 
-module p1 =
-    let solve (input: List<string>) =
-        input
-        |> Seq.map (fun s ->
-            if ((String.IsNullOrWhiteSpace >> not) s) then
-                Some(int64 s)
-            else
-                None)
-        |> Seq.fold
-            (fun acc cal ->
-                let (c, s) = acc
+let p1 (input: List<string>) =
+    input
+    |> Seq.map (fun s ->
+        if ((String.IsNullOrWhiteSpace >> not) s) then
+            Some(int64 s)
+        else
+            None)
+    |> Seq.fold
+        (fun acc cal ->
+            let (c, s) = acc
 
-                match cal with
-                | Some v -> (c + v, max (c + v) s)
-                | None -> (0L, s))
-            (0, 0)
-        |> snd
+            match cal with
+            | Some v -> (c + v, max (c + v) s)
+            | None -> (0L, s))
+        (0, 0)
+    |> snd
 
 let input = Common.readIn
-input |> p1.solve |> Common.writeOut
+input |> p1 |> Common.writeOut
